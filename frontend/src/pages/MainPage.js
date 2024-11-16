9d2b8034e50645fa984692d1a7aa532cf9ed466d
beahackman <123425454+beahackman@users.noreply.github.com>
2024-11-16 14:07:29 -0800

    LETS GET GOING

---
+++ frontend/src/pages/MainPage.js
@@ -0,0 +1,15 @@
+import "./stylesheets/MainPage.css"
+
+const MainPage = () => {
+    return(
+        <div class="container">
+            <div id="titleBar">Our App Name</div>
+            <div id="map">map</div>
+            <div id="descriptionBox">
+                description
+            </div>
+        </div>
+    )
+}
+
+export default MainPage;
