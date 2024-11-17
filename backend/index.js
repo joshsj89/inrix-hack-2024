119f928a0abafdc4b52a84ab4af85749cfa0f169
Ethan Sychangco <esychangco@gmail.com>
2024-11-16 23:27:10 -0800

    begin master function

--- backend/index.js
+++ backend/index.js
@@ -108,6 +108,71 @@ app.get('/cameras-radius', getToken, async (req, res) => {
     res.json({ cameras });
 });
 
+// Finalized endpoint for the frontend
+app.get('/camera-request', getToken, async(req, res) => {
+    const token = res.locals.token;
+
+    // not sure how data is getting sent yet, could be 4 vars
+    // and we have to pair them
+
+    // GetCamerasInBox portion
+    const corner1 = req.query.corner1;
+    const corner2 = req.query.corner2;
+
+    if (!token || !corner1 || !corner2) {
+        res.json({ error: "Token or geobox corners not found. Please provide." });
+    }
+    const cameras = getCamerasInABox(token, corner1, corner2);
+    res.json({ cameras });
+
+    /*
+{
+  "cameras": [
+    {
+      "id": "3579",
+      "latitude": "37.764720",
+      "longitude": "-122.405000"
+    },
+    {
+      "id": "4101",
+      "latitude": "37.768330",
+      "longitude": "-122.405500"
+    }
+  ]
+}
+    */
+
+    // hold onto the name, latitude, longitude
+
+    // GetCameraImage portion
+
+    // TODO: For each in cameras, all the way to the end
+    // apply a "key" as well starting from 1
+    // so loop like in c - for (int i = 0; i < len(cameras); i++)
+    const cameraId = req.query.camera_id;
+
+    if (!token || !cameraId) {
+        res.json({ error: "Camera ID or token not found. Please provide." });
+        return;
+    }
+
+    const image = await getCameraImage(token, cameraId);
+
+    // send request to 127.0.0.1:3000/bedrock-req img = image
+    const response = await fetch("http://localhost:3000/bedrock-req", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'image/jpeg'
+        },
+        body: image
+    });
+
+    const data = await response.text();
+    console.log(data);
+    res.json(data);
+
+})
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
