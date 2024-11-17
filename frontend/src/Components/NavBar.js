6dab3dc86dc240f968bcae086e63c5ef2093d00c
hilllls <hilaryle04@gmail.com>
2024-11-16 18:55:09 -0800

    logo shi

--- frontend/src/Components/NavBar.js
+++ frontend/src/Components/NavBar.js
@@ -1,10 +1,12 @@
 // src/components/Navbar.js
 import { NavLink } from 'react-router-dom';
 import './NavBar.css';  // Make sure to import your CSS file for styling
-
+import logo from '../logo.svg';
 function Navbar() {
     return (
         <nav>
+            <img src={logo} alt="Logo" className="logo" />
+
             <ul>
                 <li>
                     <NavLink to="/" className={({ isActive }) => (isActive ? 'active' : '')}>Home</NavLink>
