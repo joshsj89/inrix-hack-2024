50bd7ef11b1de5885f2e3974246ec4ab6b904528
joshsj89 <joshsj89@gmail.com>
2024-11-16 16:25:19 -0800

    Add token expiration logic

--- backend/index.js
+++ backend/index.js
@@ -3,8 +3,31 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
+let currentToken = '';
+
+// Gets/refreshes the token
+const getToken = async () => {
+    const expiration = new Date(currentToken.expires);
+    const now = new Date();
+
+    // Check if the token is still valid
+    if (currentToken && expiration > now) {
+        return currentToken.token;
+    } else {
+        try {
+            const response = await fetch('http://localhost:5000/token'); // change localhost later
+            const data = await response.json();
+    
+            currentToken = data;
+            return data.token;
+        } catch (error) {
+            console.error(error);
+        }
+    }
+}
+
 app.get('/token', async (req, res) => {
-    let url = `https://na-api.beta.inrix.com/Traffic/Inrix.ashx?Action=GetSecurityToken&vendorId=1680049421&consumerId=3466e4ef-329b-474f-b52b-a3818e9df6b6&format=json`; //
+    const url = `https://na-api.beta.inrix.com/Traffic/Inrix.ashx?Action=GetSecurityToken&vendorId=1680049421&consumerId=3466e4ef-329b-474f-b52b-a3818e9df6b6&format=json`;
 
     const options = {
         method: 'GET'
@@ -21,4 +44,4 @@ app.get('/token', async (req, res) => {
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
-})
+});
